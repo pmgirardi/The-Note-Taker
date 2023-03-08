f40a664998ac2ba1bb7305724eef1cffc7b91d7b
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-08 15:46:47 -0800

    added HTML routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,49 @@
-// Set up variables to import express
+// Import express.js 
 const express = require('express');
 
-// Create an instance of the express app
+//Import path node package to enable the finding of path files
+
+const path = require('path');
+
+// Invoke express and store within app
+
 const app = express();
 
+const PORT = 3001;
 
-//import routes apiRoutes and htmlRoutes
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-const PORT = process.env.PORT || 3001;
+// Middleware to request from public folder
+
+app.use(express.static('public'));
+
+// Routes 
+
+// Route to home 'public' folder
+ app.get('/', (req,res) => res.send('Navigate to /send or routes'));
+
+ // Get request for index.html file
+
+ app.get('/index', (req,res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+    );
+
+ // Get request for notes.html file
+
+ app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+    );
+
+// app.listen to callback function once express is invoked
+
+app.listen(PORT, () =>
+    console.log(`listening at http://localhost:$(PORT)`)
+    );
+
+
+/* On the back end, the application should include a `db.json` file that will be used to store and retrieve notes using the `fs` module.
+
+
+The following API routes should be created:
+
+* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
+* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
