604e67d501ebe141e662c8b770d46345dd9d3eba
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-08 08:29:27 -0800

    added require for apiRoutes andn htmlRoutes

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,11 @@
 // Set up variables to import express
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
-
 // Create an instance of the express app
 const app = express();
+
+
+//import routes apiRoutes and htmlRoutes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
