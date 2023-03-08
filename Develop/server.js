c88696e4345ee8b893b6e9ccdae6c0ff82a6af18
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-08 08:17:02 -0800

    set up variables and instance of express app in server.js file

---
+++ Develop/server.js
@@ -0,0 +1,7 @@
+// Set up variables to import express
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+// Create an instance of the express app
+const app = express();
